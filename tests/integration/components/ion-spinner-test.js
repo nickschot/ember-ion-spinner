70678330774724d4e5c90e4b6a352ca01bb9e2dd
nickschot <nickschot@gmail.com>
2020-08-22 22:05:40 +0200

    fix tests/lint

--- tests/integration/components/ion-spinner-test.js
+++ tests/integration/components/ion-spinner-test.js
@@ -13,14 +13,5 @@ module('Integration | Component | ion-spinner', function(hooks) {
     await render(hbs`<IonSpinner />`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <IonSpinner>
-        template block text
-      </IonSpinner>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
